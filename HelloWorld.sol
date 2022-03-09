01f26e68f57f77d26ab978ef7aa20d6ea9ae356d
"creativeyute" <"creative.yute@gmail.com">
2022-03-10 00:37:57 +0100

    A "Hello World" smart contract with a constructor, at least two functions, and a state variable, and deployed to the Rinkeby Testnet(Ethereum Blockchain)

---
+++ HelloWorld.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+   string public message;
+   
+   constructor() {message = "decentraland";
+   }
+
+   function setMessage(string memory _message) public {
+      message = _message;
+   }
+
+   function viewMessage() public view returns(string memory){
+      return message;
+   }
+}
